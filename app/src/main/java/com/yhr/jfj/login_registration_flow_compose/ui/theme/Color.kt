2e09fa64b0c4bf9524bc20c6dc1db3ce358b110c
RiJo <102893295+yhrjfj@users.noreply.github.com>
2023-12-25 15:05:32 +0600

    Add color

--- app/src/main/java/com/yhr/jfj/login_registration_flow_compose/ui/theme/Color.kt
+++ app/src/main/java/com/yhr/jfj/login_registration_flow_compose/ui/theme/Color.kt
@@ -9,3 +9,11 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+// From Mine
+val Primary = Color(0xFF92A3FD)
+val Secondary = Color(0xFF9DCEFF)
+val TextColor = Color(0xFF1D1617)
+val AccentColor = Color(0xFFC58BF2)
+val GrayColor = Color(0xFF7B6F72)
+val WhiteColor = Color(0xFFFFFFFF)
