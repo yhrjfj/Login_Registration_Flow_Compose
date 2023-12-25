b96463bb0f91902f5fef4cc20f781f02d41bab4b
RiJo <102893295+yhrjfj@users.noreply.github.com>
2023-12-25 16:22:43 +0600

    Add some shape value

---
+++ app/src/main/java/com/yhr/jfj/login_registration_flow_compose/ui/theme/ComponentsShape.kt
@@ -0,0 +1,11 @@
+package com.yhr.jfj.login_registration_flow_compose.ui.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Shapes
+import androidx.compose.ui.unit.dp
+
+val ComponentsShape = Shapes(
+    small = RoundedCornerShape(4.dp),
+    medium = RoundedCornerShape(8.dp),
+    large = RoundedCornerShape(0.dp)
+)
