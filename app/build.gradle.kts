aded90643258fd1f3139f1d43ecfa454b6ab668f
RiJo <102893295+yhrjfj@users.noreply.github.com>
2023-12-31 22:43:40 +0600

    Add dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,7 +53,7 @@ dependencies {
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-    implementation("androidx.activity:activity-compose:1.8.1")
+    implementation("androidx.activity:activity-compose:1.8.2")
     implementation(platform("androidx.compose:compose-bom:2023.08.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
@@ -66,4 +66,11 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+    // Icons
+    // Define the compose_version variable
+    val compose_version = "1.3.1"
+    // Icons
+    //noinspection GradleDependency
+    implementation ("androidx.compose.material:material-icons-extended:$compose_version")
 }
