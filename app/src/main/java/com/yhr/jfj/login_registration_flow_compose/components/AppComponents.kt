41b6d99057c049e5771b438933049a7f3f3bf722
RiJo <102893295+yhrjfj@users.noreply.github.com>
2023-12-31 20:11:09 +0600

    Change TextComponent property

--- app/src/main/java/com/yhr/jfj/login_registration_flow_compose/components/AppComponents.kt
+++ app/src/main/java/com/yhr/jfj/login_registration_flow_compose/components/AppComponents.kt
@@ -6,68 +6,69 @@ import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Person
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.yhr.jfj.login_registration_flow_compose.R
 import com.yhr.jfj.login_registration_flow_compose.ui.theme.BgColor
 import com.yhr.jfj.login_registration_flow_compose.ui.theme.ComponentsShape
 import com.yhr.jfj.login_registration_flow_compose.ui.theme.Primary
 import com.yhr.jfj.login_registration_flow_compose.ui.theme.TextColor
-import androidx.compose.ui.R as R1
 
 @Composable
-fun NormalTextComponent(
-    value: String
+fun TextComponent(
+    value: String,
+    minHeight: Dp = 40.dp,
+    fontSize: TextUnit = 24.sp,
+    fontWeight: FontWeight = FontWeight.Normal,
+    fontStyle: FontStyle = FontStyle.Normal
 ) {
     Text(
         text = value,
         modifier = Modifier
             .fillMaxWidth()
-            .heightIn(min = 40.dp),
+            .heightIn(min = minHeight),
         style = TextStyle(
-            fontSize = 24.sp,
-            fontWeight = FontWeight.Normal,
-            fontStyle = FontStyle.Normal
+            fontSize = fontSize,
+            fontWeight = fontWeight,
+            fontStyle = fontStyle
         ),
         color = TextColor,
         textAlign = TextAlign.Center
     )
 }
 
-@Composable
-fun HeadingTextComponent(
-    value: String
-) {
-    Text(
-        text = value,
-        modifier = Modifier
-            .fillMaxWidth()
-            .heightIn(),
-        style = TextStyle(
-            fontSize = 30.sp,
-            fontWeight = FontWeight.Bold,
-            fontStyle = FontStyle.Normal
-        ),
-        color = TextColor,
-        textAlign = TextAlign.Center
-    )
-}
+//@Composable
+//fun HeadingTextComponent(
+//    value: String
+//) {
+//    Text(
+//        text = value,
+//        modifier = Modifier
+//            .fillMaxWidth()
+//            .heightIn(),
+//        style = TextStyle(
+//            fontSize = 30.sp,
+//            fontWeight = FontWeight.Bold,
+//            fontStyle = FontStyle.Normal
+//        ),
+//        color = TextColor,
+//        textAlign = TextAlign.Center
+//    )
+//}
 
 @Composable
 fun MyTextFieldComponent(
--- app/src/main/java/com/yhr/jfj/login_registration_flow_compose/screens/SignUpScreen.kt
+++ app/src/main/java/com/yhr/jfj/login_registration_flow_compose/screens/SignUpScreen.kt
@@ -9,12 +9,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.yhr.jfj.login_registration_flow_compose.R
-import com.yhr.jfj.login_registration_flow_compose.components.HeadingTextComponent
 import com.yhr.jfj.login_registration_flow_compose.components.MyTextFieldComponent
-import com.yhr.jfj.login_registration_flow_compose.components.NormalTextComponent
+import com.yhr.jfj.login_registration_flow_compose.components.TextComponent
 
 @Composable
 fun SignUpScreen() {
@@ -28,9 +29,18 @@ fun SignUpScreen() {
             modifier = Modifier
                 .fillMaxSize()
         ) {
-            NormalTextComponent(value = stringResource(id = R.string.hello))
-            HeadingTextComponent(value = stringResource(id = R.string.create_account))
-            MyTextFieldComponent(labelValue = stringResource(id = R.string.first_name))
+            TextComponent(
+                value = stringResource(id = R.string.hello)
+            )
+            TextComponent(
+                value = stringResource(id = R.string.create_account),
+                minHeight = 40.dp,
+                fontSize = 30.sp,
+                fontWeight = FontWeight.Bold
+            )
+            MyTextFieldComponent(
+                labelValue = stringResource(id = R.string.first_name)
+            )
         }
     }
 }
